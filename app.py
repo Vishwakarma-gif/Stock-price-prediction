f5112d7e9d206a9f06df59c9fee00e0107b62ab5
baijuji <108528842+baijuji@users.noreply.github.com>
2022-07-26 16:39:29 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,74 @@
+# pip install streamlit fbprophet yfinance plotly
+import streamlit as st
+
+import yfinance as yf
+from prophet import Prophet
+from prophet.plot import plot_plotly
+from plotly import graph_objs as go
+
+st.image("compunnel.png",width=100)
+
+st.title('Stock Forecast App')
+
+stocks = ('AAPL', 'MSFT', 'UNH','GOOG','AMZN') 
+selected_stock = st.selectbox('Select dataset for prediction', stocks)
+
+n_years = st.slider('Years of prediction:', 1, 4)
+period = n_years * 365
+
+
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker,period = '5y')
+    data.reset_index(inplace=True)
+    return data
+
+	
+data_load_state = st.text('Loading data...')
+data = load_data(selected_stock)
+data_load_state.text('Loading data... done!')
+
+st.subheader('Raw data')
+st.write(data.tail())
+
+# Plot raw data
+def plot_raw_data():
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+	st.plotly_chart(fig)
+	
+plot_raw_data()
+
+# Predict forecast with Prophet.
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+# Show and plot forecast
+st.subheader('Forecast data')
+st.write(forecast.tail())
+    
+csv=forecast[['ds','yhat_upper','yhat_lower','yhat']].to_csv(index=False).encode("utf-8")
+st.download_button(
+         label="Download data as CSV",
+         data=csv,
+         file_name='forecaste.csv',
+         mime='text/csv',
+         )
+
+
+
+st.subheader(f'Forecast plot for {n_years} years')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.subheader("Forecast components")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
+Footer
