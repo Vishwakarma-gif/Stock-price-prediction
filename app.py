75ce96da3b10ce3c195339caa0e109ae99e95780
baijuji <108528842+baijuji@users.noreply.github.com>
2022-07-27 23:20:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,63 +1,136 @@
-# pip install streamlit fbprophet yfinance plotly
-# import packages and alias
-import numpy as np # data arrays
-import pandas as pd # data structure and data analysis
-import matplotlib as plt # data visualization
-import matplotlib.pyplot as plt
-import datetime as dt # date time
-import xgboost as xgb
-from xgboost import plot_importance, plot_tree
-from sklearn.metrics import mean_squared_error, mean_absolute_error
-import seaborn as sns
-#import warnings
-#warnings.filterwarnings("ignore")
-%matplotlib inline
-sns.set_theme()
-pd.set_option('display.max_columns', None)
-import math
-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
 import yfinance as yf
+from keras.models import load_model
+import streamlit as st
+import pandas_datareader as data
 
+st.set_page_config(page_title = "Compunnel digital")
 st.image("compunnel.png",width=100)
 
-st.title('Stock Forecast App')
-
+st.title('Stock Trend Prediction')
 stocks = ('AAPL', 'MSFT', 'UNH','GOOG','AMZN') 
-selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
-@st.cache
-def load_data(ticker):
-    data = yf.download(ticker,period = '5y')
-    data.reset_index(inplace=True)
-    return data
+user_input = st.selectbox('Select dataset for prediction', stocks)
 
-	
-data_load_state = st.text('Loading data...')
-data = load_data(selected_stock)
-data_load_state.text('Loading data... done!')
 
-st.subheader('Raw data')
-st.write(data.tail())
+start = '2017-01-01'
 
-# Plot raw data
-def plot_raw_data():
-	fig = go.Figure()
-	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
-	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
-	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
-	st.plotly_chart(fig)
-	
-plot_raw_data()
+df = data.DataReader(user_input,'yahoo',start)
 
-AAL_df['Close_first']=AAL_df['Close']-AAL_df['Close'].shift(1)
+#Describing Data
 
-AAL_df.drop(['Close','Adj Close','Open','High','Low','EMA_0.1','Volume'],axis=1,inplace=True)
-AAL_df.dropna(inplace=True)
+st.write(df.describe())
 
-train=AAL_df.loc[AAL_df.index<'2021-05-01']
-test=AAL_df.loc[AAL_df.index>='2021-05-01']
+#Visualizations
 
-reg=load_model('/content/xgboost.h5')
-pred=reg.predict(X_test)
+st.subheader('Closing Price vs Time Chart')
+fig = plt.figure(figsize=(12,6))
+plt.plot(df.Close,label ='Closing Price')
+plt.legend()
+plt.xlabel('Time')
+plt.ylabel('Price')
+st.pyplot(fig)
 
-st.write('pred')
+st.subheader('Closing Price vs Time Chart with 100MA')
+ma100 = df.Close.rolling(100).mean()
+fig = plt.figure(figsize=(12,6))
+plt.plot(df.Close, 'b',label ='Closing Price')
+plt.plot(ma100,'r',label ='MA100')
+plt.legend()
+plt.xlabel('Time')
+plt.ylabel('Price')
+st.pyplot(fig)
+
+st.subheader('Closing Price vs Time Chart with 200MA')
+ma200 = df.Close.rolling(200).mean()
+fig = plt.figure(figsize=(12,6))
+plt.plot(ma100, 'r',label ='MA100')
+plt.plot(ma200, 'g',label ='MA200')
+plt.plot(df.Close, 'b',label ='Closing Price')
+plt.legend()
+plt.xlabel('Time')
+plt.ylabel('Price')
+st.pyplot(fig)
+
+# Splitting Data into Training and Testing
+
+data_training = pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
+data_testing = pd.DataFrame(df['Close'][int(len(df)*0.70):int(len(df))])
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0,1))
+
+data_training_array = scaler.fit_transform(data_training)
+
+
+#Load Model
+
+model = load_model('keras_model (1).h5')
+
+#Testing Part
+
+past_100_days = data_training.tail(100)
+final_df = past_100_days.append(data_testing,ignore_index =True)
+input_data = scaler.fit_transform(final_df)
+
+x_test = []
+y_test = []
+
+for i in range(100, input_data.shape[0]):
+    x_test.append(input_data[i-100:i])
+    y_test.append(input_data[i,0])
+x_test , y_test = np.array(x_test), np.array(y_test)
+
+#Making Predictions
+
+y_predicted = model.predict(x_test)
+
+#scaler = scaler.scale_
+
+#scale_factor = 1/scaler[0]
+#y_predicted = y_predicted * scale_factor
+#y_test = y_test * scale_factor
+
+y_test = y_test.reshape(-1,1)
+y_predicted = scaler.inverse_transform(y_predicted)
+y_test = scaler.inverse_transform(y_test)
+
+yp = pd.DataFrame(y_predicted,columns=(['Predicted']))
+yt = pd.DataFrame(y_test,columns=(['Actual']))
+y = pd.concat([yt,yp],axis = 1)
+dd = data.DataReader(user_input,'yahoo',start)
+dd = dd[-421:]
+d1 = dd.index
+#d2 = d1.split('T')
+y1 = y.set_index(d1)
+st.subheader('Actual Vs Predicted Prices')
+st.write(y1.tail(5))
+
+#Final Graph
+st.subheader('Actual Vs Predicted Chart')
+fig2 = plt.figure(figsize=(12,6))
+plt.plot(y_test, 'b', label = 'Original Price')
+plt.plot(y_predicted, 'r', label = 'Predicted Price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend()
+st.pyplot(fig2)
+
+st.subheader('Metrics')
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+scores = [['Mean Absolute Error', mean_absolute_error(y_test,y_predicted)], ['Mean Squared Error', mean_squared_error(y_test, y_predicted)], ['Root Mean Squared Error', np.sqrt(mean_squared_error(y_test, y_predicted))],['R2 Score', r2_score(y_test, y_predicted)]]
+scores_f = pd.DataFrame(scores, columns=['Metrics', 'Scores'])
+st.write(scores_f)
+
+
+
+
+
+
+
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
