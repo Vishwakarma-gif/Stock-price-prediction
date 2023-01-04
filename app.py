e16f5f7688fdef77353f64478c332da907dc071b
Baijnath vishwakarma <108528842+baijuji@users.noreply.github.com>
2023-01-04 19:59:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,131 +1,72 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt 
-import yfinance as yf
-from keras.models import load_model
 import streamlit as st
-import pandas_datareader as data
 
-st.set_page_config(page_title = "Compunnel digital")
+import yfinance as yf
+from prophet import Prophet
+from prophet.plot import plot_plotly
+from plotly import graph_objs as go
+
 st.image("compunnel.png",width=100)
 
-st.title('Stock Trend Prediction')
+st.title('Stock Forecast App')
+
 stocks = ('AAPL', 'MSFT', 'UNH','GOOG','AMZN') 
+selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
-user_input = st.selectbox('Select dataset for prediction', stocks)
+n_years = st.slider('Years of prediction:', 1, 4)
+period = n_years * 365
 
 
+@st.cache
 def load_data(ticker):
     data = yf.download(ticker,period = '5y')
     data.reset_index(inplace=True)
     return data
 
-#Describing Data
+	
+data_load_state = st.text('Loading data...')
+data = load_data(selected_stock)
+data_load_state.text('Loading data... done!')
 
-st.write(df.describe())
+st.subheader('Raw data')
+st.write(data.tail())
 
-#Visualizations
+# Plot raw data
+def plot_raw_data():
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+	st.plotly_chart(fig)
+	
+plot_raw_data()
 
-st.subheader('Closing Price vs Time Chart')
-fig = plt.figure(figsize=(12,6))
-plt.plot(df.Close,label ='Closing Price')
-plt.legend()
-plt.xlabel('Time')
-plt.ylabel('Price')
-st.pyplot(fig)
+# Predict forecast with Prophet.
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
-st.subheader('Closing Price vs Time Chart with 100MA')
-ma100 = df.Close.rolling(100).mean()
-fig = plt.figure(figsize=(12,6))
-plt.plot(df.Close, 'b',label ='Closing Price')
-plt.plot(ma100,'r',label ='MA100')
-plt.legend()
-plt.xlabel('Time')
-plt.ylabel('Price')
-st.pyplot(fig)
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
 
-st.subheader('Closing Price vs Time Chart with 200MA')
-ma200 = df.Close.rolling(200).mean()
-fig = plt.figure(figsize=(12,6))
-plt.plot(ma100, 'r',label ='MA100')
-plt.plot(ma200, 'g',label ='MA200')
-plt.plot(df.Close, 'b',label ='Closing Price')
-plt.legend()
-plt.xlabel('Time')
-plt.ylabel('Price')
-st.pyplot(fig)
-
-# Splitting Data into Training and Testing
-
-data_training = pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
-data_testing = pd.DataFrame(df['Close'][int(len(df)*0.70):int(len(df))])
-
-from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler(feature_range=(0,1))
-
-data_training_array = scaler.fit_transform(data_training)
-
-
-#Load Model
-
-model = load_model('keras_model (1).h5')
-
-#Testing Part
-
-past_100_days = data_training.tail(100)
-final_df = past_100_days.append(data_testing,ignore_index =True)
-input_data = scaler.fit_transform(final_df)
-
-x_test = []
-y_test = []
-
-for i in range(100, input_data.shape[0]):
-    x_test.append(input_data[i-100:i])
-    y_test.append(input_data[i,0])
-x_test , y_test = np.array(x_test), np.array(y_test)
-
-#Making Predictions
-
-y_predicted = model.predict(x_test)
-
-#scaler = scaler.scale_
-
-#scale_factor = 1/scaler[0]
-#y_predicted = y_predicted * scale_factor
-#y_test = y_test * scale_factor
-
-y_test = y_test.reshape(-1,1)
-y_predicted = scaler.inverse_transform(y_predicted)
-y_test = scaler.inverse_transform(y_test)
-
-yp = pd.DataFrame(y_predicted,columns=(['Predicted']))
-yt = pd.DataFrame(y_test,columns=(['Actual']))
-y = pd.concat([yt,yp],axis = 1)
-dd = data.DataReader(user_input,'yahoo',start)
-dd = dd[-421:]
-d1 = dd.index
-#d2 = d1.split('T')
-y1 = y.set_index(d1)
-st.subheader('Actual Vs Predicted Prices')
-st.write(y1.tail(5))
-
-#Final Graph
-st.subheader('Actual Vs Predicted Chart')
-fig2 = plt.figure(figsize=(12,6))
-plt.plot(y_test, 'b', label = 'Original Price')
-plt.plot(y_predicted, 'r', label = 'Predicted Price')
-plt.xlabel('Time')
-plt.ylabel('Price')
-plt.legend()
-st.pyplot(fig2)
-
-st.subheader('Metrics')
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
-scores = [['Mean Absolute Error', mean_absolute_error(y_test,y_predicted)], ['Mean Squared Error', mean_squared_error(y_test, y_predicted)], ['Root Mean Squared Error', np.sqrt(mean_squared_error(y_test, y_predicted))],['R2 Score', r2_score(y_test, y_predicted)]]
-scores_f = pd.DataFrame(scores, columns=['Metrics', 'Scores'])
-st.write(scores_f)
+# Show and plot forecast
+st.subheader('Forecast data')
+st.write(forecast.tail())
+    
+csv=forecast[['ds','yhat_upper','yhat_lower','yhat']].to_csv(index=False).encode("utf-8")
+st.download_button(
+         label="Download data as CSV",
+         data=csv,
+         file_name='forecaste.csv',
+         mime='text/csv',
+         )
 
 
 
+st.subheader(f'Forecast plot for {n_years} years')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
 
-
+st.subheader("Forecast components")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
