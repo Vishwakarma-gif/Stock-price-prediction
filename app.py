62241eb5efd5089785cb6f9e5a8bb26a1b7706bc
Baijnath vishwakarma <108528842+baijuji@users.noreply.github.com>
2023-01-04 20:06:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,6 +67,4 @@ st.subheader(f'Forecast plot for {n_years} years')
 fig1 = plot_plotly(m, forecast)
 st.plotly_chart(fig1)
 
-st.subheader("Forecast components")
-fig2 = m.plot_components(forecast)
-st.write(fig2)
+
